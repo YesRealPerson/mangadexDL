7f6ca7b35dadda5eeeb65e4e6a74a8e2b9af0e7a
YesRealPerson <stephen-park@outlook.com>
2021-08-17 16:33:53 -0700

    added some shit

--- manga.py
+++ manga.py
@@ -12,7 +12,7 @@ def dl_img(url, file_path, file_name):
 
 while(True):
     search = input("What is the name of the manga you are looking for?  ")
-    manga_list = api.get_manga_list(self=api, title=search)
+    manga_list = api.get_manga_list(self=api,title=search)
 # manga = api.view_manga_by_id(self=mangadex.Api,id="c086153a-0162-412a-9914-a7b2633d0cd3")
 # volumes = api.get_manga_volumes_and_chapters(self=api, id="c086153a-0162-412a-9914-a7b2633d0cd3")
 # returns dictionary usage: volumes["*VOLUME NUMBER*"]
@@ -24,7 +24,7 @@ while(True):
             manga = api.view_manga_by_id(self=api, id=x.id)
             break
     if(found!="y"):
-        break
+        pass
     mangadesc = ''.join(i for i in manga.description if not i.isdigit())
     print(manga.title["en"])
     print(manga.description["en"])
@@ -34,16 +34,21 @@ while(True):
         chapters = api.chapter_list(self=api, manga=manga.id, volume=str(x))
         print("Volume: "+str(x))
         for y in chapters:
+            #print("NOT EN - Chapter:"+str(int(y.chapter))+" "+y.title)
             if(y.translatedLanguage=="en"):
                 print("Chapter: "+str(int(y.chapter))+" "+y.title)
                 chapterDict.update({str(int(y.chapter)):y.id})
-    chapterNumber = input("What chapter do you want to download?   ")
-    pages = api.get_chapter(self=api, id=str(chapterDict[chapterNumber])).fetch_chapter_images()
-    # print(pages)
-    x=0
-    title = re.sub(r'\W+', '', str(manga.title))+str(chapterNumber)
-    os.makedirs(title)
-    for src in pages:
-        name = title+" "+str(x)
-        dl_img(src, title+"/", name)
-        x+=1
+    downloading = True
+    while(downloading):
+        chapterNumber = input("What chapter do you want to download?   ")
+        pages = api.get_chapter(self=api, id=str(chapterDict[chapterNumber])).fetch_chapter_images()
+        x=0
+        title = re.sub(r'\W+', '', str(manga.title))+str(chapterNumber)
+        os.makedirs(title)
+        for src in pages:
+            name = title+" "+str(x)
+            dl_img(src, title+"/", name)
+            x+=1
+        yesno = input("stop? (type y for yes anything else for no)")
+        if(yesno=="y"):
+            downloading = False
